f5e31551a9d87c25082f383292486a97a52520f0
Joel Martinsson <Joel.martinsson.molk@gmail.com>
2017-11-28 15:58:34 +0100

    modified makefile in AVR-Test-Library

--- AVR-Test-Library/func.c
+++ AVR-Test-Library/func.c
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-//#include "../../Libs/unity/extras/fixture/src/unity_fixture.h"
-=======
-#include "Libs/unity/extras/fixture/src/unity_fixture.h"
->>>>>>> a59a2d67f3361e154d26e3f9d3d1618d243cd6db
+#include  "..\Libs\unity\extras\fixture\src\unity_fixture.h"
--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
+#include  "Libs\unity\extras\fixture\src\unity_fixture.h"
 
 int main(){
   DDRD |= (1 << PD0);
   while(1){
     PORTD ^= 1 << PD0;
-    _delay_ms(3000); 
+    _delay_ms(3000);
   }
   return 0;
 }
