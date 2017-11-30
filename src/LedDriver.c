9cb06849aa707b5127d2f6711cb2807a9683d6ba
Joel Martinsson <Joel.martinsson.molk@gmail.com>
2017-11-30 18:14:32 +0100

    added turnoffany func

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -2,6 +2,9 @@
 
 static uint16_t *ledsAddress;
 
+enum{ALL_LEDS_ON = ~0, ALL_LEDS_OFF = ~ALL_LEDS_ON};
+
+
 void LedDriver_Create(uint16_t *address){
   ledsAddress = address;
   *ledsAddress = 0;
@@ -20,5 +23,9 @@ void LedDriver_TurnOn(int ledNumber){
 }
 
 void LedDriver_TurnOff(int ledNumber){
-  *ledsAddress = 0;
+  *ledsAddress &= ~(convertLedNumberToBit(ledNumber));
+}
+
+void LedDriver_TurnAllOn(void){
+  *ledsAddress = ALL_LEDS_ON;
 }
--- src/LedDriver.h
+++ src/LedDriver.h
@@ -4,3 +4,4 @@ void LedDriver_Create();
 void LedDriver_Destroy(void);
 void LedDriver_TurnOn(int ledNummbr);
 void LedDriver_TurnOff(int ledNumber);
+void LedDriver_TrunAllOn(void);
