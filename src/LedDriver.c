0f6d60585c575bf60bc2e47d6dfdccec15faf630
Joel Martinsson <Joel.martinsson.molk@gmail.com>
2017-11-30 17:58:01 +0100

    added bitshift func

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -11,8 +11,12 @@ void LedDriver_Destroy(void){
 
 }
 
+static uint16_t convertLedNumberToBit(int ledNumber){
+  return 1 << (ledNumber - 1);
+}
+
 void LedDriver_TurnOn(int ledNumber){
-    *ledsAddress = 1;
+  *ledsAddress |= convertLedNumberToBit(ledNumber);
 }
 
 void LedDriver_TurnOff(int ledNumber){
